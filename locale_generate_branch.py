2dded9db29af2dbe8e9728058c7d3b40005e201c
diSabler <dissy@ya.ru>
2013-05-09 13:44:59 +0400

    add: generate locale for help

--- locale_generate_branch.py
+++ locale_generate_branch.py
@@ -21,9 +21,14 @@
 #                                                                             #
 # --------------------------------------------------------------------------- #
 
-import os,re
+import os,re,time
 
 def L(t): return t
+
+def GT(t):
+	if t in ['disco_exclude','lfm_api']: return ''
+	elif t in ['html_logs_enable']: return 0
+	else: return None
 
 def readfile(filename):
 	fp = file(filename)
@@ -37,6 +42,7 @@ def writefile(filename, data):
 	fp.close()
 
 def regenerate(ptl,locale_name):
+	global execute,exec_yes,comms
 	def L(text):
 		if not len(text): return text
 		try: return locales[text]
@@ -67,8 +73,11 @@ def regenerate(ptl,locale_name):
 	error_count = 0
 
 	for tmp in file_list:
+		execute,exec_yes,comms = [],[],[]
+		if 'plugin' in tmp: execfile(tmp,globals())
 		body = str(readfile(tmp)).replace('\\\'','\'').replace('\\\"','\"').replace('\\\\','\\')
 		tm = re.findall(regex,body,re.S+re.U)
+		for t in execute+exec_yes+comms: tm.append(t[4].replace('\n','\\n'))
 		tm2 = re.findall('# translate: (.*)',body)
 		if tm2: tm2 = tm2[0].split(',')
 		if tm or tm2:
@@ -87,19 +96,53 @@ def regenerate(ptl,locale_name):
 					result += '%s\t%s\n' % (tmp2,loc)
 				result += '\n'
 
-	
 	execfile(path_to_mainpy,globals())
+	tm2 = []
 	for t in config_prefs.keys():
-		localez.append(config_prefs[t][0])
-		localez.append(config_prefs[t][1])
+		if config_prefs[t][0] not in localez:
+			localez.append(config_prefs[t][0])
+			tm2.append(config_prefs[t][0])
+		if config_prefs[t][1] not in localez:
+			localez.append(config_prefs[t][1])
+			tm2.append(config_prefs[t][1])
 		
 	for t in config_groups:
-		localez.append(t[0])
+		if t[0] not in localez: 
+			localez.append(t[0])
+			tm2.append(t[0])
+		
+	for t in owner_prefs.keys():
+		if owner_prefs[t][0] not in localez: 
+			localez.append(owner_prefs[t][0])
+			tm2.append(owner_prefs[t][0])
+		
+	for t in owner_groups:
+		if t[0] not in localez:
+			localez.append(t[0])
+			tm2.append(t[0])
+
+	if tm2:
+		result += '%s%s\n' % (file_mark,'Generic')
+		for tmp2 in tm2:
+			tr = L(tmp2)
+			if tmp2 == tr:
+				error_count += 1
+				loc = locale_mark
+			else: loc = tr
+			result += '%s\t%s\n' % (tmp2,loc)
+		result += '\n'
+			
+	result += '# write locale file: %s' % locale_name
+	result += '\n# total: %s' % len(localez)
 
 	msg = 'write locale file: %s' % locale_name
-	if error_count: msg += '\nmissed translations: %s\n' % error_count
-	else: msg += '\nregenarated without mistakes!\n'
-
+	if error_count:
+		msg += '\nmissed translations: %s\n' % error_count
+		result += '\n# missed translations: %s\n' % error_count
+	else:
+		msg += '\nregenarated without mistakes!\n'
+		result += '\n# regenarated without mistakes!\n'
+		
 	return msg, result, len(localez)
 
 path_to_source = '../branch'
@@ -110,6 +153,9 @@ path_to_mainpy = '%s/main.py' % path_to_plugins
 smiles_dirs = []
 data_folder = path_to_source + '/data/%s'
 loc_folder 	= data_folder % 'locales/%s.txt'
+log_folder 	= data_folder % 'conflogs/%s'
+public_log = log_folder % 'chatlogs/%s'
+system_log = log_folder % 'syslogs/%s'
 	
 locale_mark = 'NO_TRANSLATE'
 file_mark = '# file: '
@@ -117,6 +163,10 @@ locale_header = '''#---------------------------------------------
 #	  Autogenerate for locales
 #	(c) Disabler Production Lab.
 #---------------------------------------------'''
+
+smiles_dirs_case = logs_css_path = ''
+execute,exec_yes,comms = [],[],[]
+
 
 if __name__ == "__main__" :
 	print '\n',locale_header.replace('#',''),'\n'
